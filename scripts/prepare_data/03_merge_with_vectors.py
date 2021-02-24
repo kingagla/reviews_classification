caef63d5a1b07cdc56555b0d6637f3d979eac36f
kingagla <kingagla@gmail.com>
2021-02-24 21:59:12 +0100

    merge reviews with vectors

---
+++ scripts/prepare_data/03_merge_with_vectors.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def review_vector_merge(reviews):
+    vectors = []
+    for i in range(reviews.shape[0]):
+        try:
+            vec = pd.read_pickle(f'./data/roberta_pickles/{i}.pkl')
+            vectors.append((i, vec))
+        except Exception as e:
+            print(e)
+
+    vals = [x[1] for x in vectors]
+    ids = [x[0] for x in vectors]
+
+    df = pd.DataFrame(vals, columns=[f'Vec_{i}' for i in range(1024)], index=ids)
+    reviews = reviews.merge(df, left_index=True, right_index=True)
+    reviews.to_pickle('./data/reviews_with_vec.pickle')
+
+
+if __name__ == '__main__':
+    reviews = pd.read_csv('../../data/reviews.csv')
+    review_vector_merge(reviews)
