00d58a3e71750b5588e7cd126534d99c5d1ab213
kingagla <kingagla@gmail.com>
2021-02-24 21:56:42 +0100

    web scrapping update

--- scripts/prepare_data/01_web_scraping.py
+++ scripts/prepare_data/01_web_scraping.py
@@ -2,38 +2,49 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import time
+import pandas as pd
 
 
 def get_review(url):
     # open csv
-    file = open('./data/reviews.csv', 'a', encoding='utf-8')
+    file = open('../../data/reviews.csv', 'a', encoding='utf-8')
     # define settings for csv
     writer = csv.writer(file, delimiter=';', lineterminator='\n')
     # get page as xml file
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
+    # extract opinions' text
     revs_text = [item.text.replace('\n', ' ').replace('\r',' ') for item in soup.find_all('div', class_='revz_txt')]
+    # extract stars from opinions
     stars = (float(item.text.split('/')[0]) for item in soup.find_all('span', class_='review_badge'))
+    # extract information if opinion is positive, negative or neutral
     revs = (item.get('class')[1] for item in soup.find_all('span', class_='review_badge'))
 
-    # write rows to document
+    # write row to document
     for i, (star, rev, text) in enumerate(zip(stars, revs, revs_text)):
         writer.writerow([star, rev, text])
     file.close()
     return i
 
 
-if __name__ == '__main__':
-    # download opinions - limiting to 20 000 due to my computer power
+def main():
+    # download opinions - limiting to 35 000
     n = 0
     page_num = 9073
-    max_opinions = 20000
+    max_opinions = 35000
     while n <= max_opinions:
         start_time = time.time()
         # define web page for scrapping
         page = f'https://www.opineo.pl/opinie/dhl-com-pl/{page_num}#opinie'
-        n += get_review(page)
+        n += get_review(page)  # current number of downloaded opinions
         page_num -= 1
         print(n, page_num, time.time()-start_time)
 
+    reviews = pd.read_csv('../../data/reviews.csv', sep=';', names=['Star', 'Information', 'Opinion'], engine='python',
+                          encoding='utf-8')
+    reviews.to_csv('./data/reviews.csv', index=False)
+    reviews.to_pickle('./data/reviews.pickle')
 
+
+if __name__ == '__main__':
+    main()
